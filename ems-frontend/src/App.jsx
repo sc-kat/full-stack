5d469858464cb6411a2b4c3ce3ad9b471454caed
sckat <ala_cl@yahoo.com>
2024-07-05 13:34:29 +0300

    created EmployeeComponent and configured Routing

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
+import EmployeeComponent from './components/EmployeeComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/* http://localhost:3000 */}
+          <Route path='/' element={<ListEmployeeComponent />}></Route>
+          {/* http://localhost:3000/employees */}
+          <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+          {/* http://localhost:3000/add-employee */}
+          <Route path='/add-employee' element={<EmployeeComponent />}></Route>
+
+        </Routes>
+
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
