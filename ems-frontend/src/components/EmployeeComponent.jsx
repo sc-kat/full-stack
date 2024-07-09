6347e4b8ee700e9f03af92e5b7292fb03f0f6de1
sckat <ala_cl@yahoo.com>
2024-07-09 16:37:03 +0300

    delete emplouee rest api implemented

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,126 +1,163 @@
-import { useState } from "react";
-import { createEmployee } from "../services/EmployeeService";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { createEmployee, getEmployee, updateEmployee } from "../services/EmployeeService";
+import { useNavigate, useParams } from "react-router-dom";
 
 const EmployeeComponent = () => {
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
 
-  const [errors, setErrors] = useState({
-    firstName: '',
-    lastName: '',
-    email: ''
-  })
+    const {id} = useParams();
+    const [errors, setErrors] = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
 
-  const navigator = useNavigate();
+    const navigator = useNavigate();
 
-  function saveEmployee(e) {
-    e.preventDefault();
+    useEffect(() => {
 
-    if(validateForm()) {
-        const employee = { firstName, lastName, email };
-    console.log(employee);
+        if(id){
+            getEmployee(id).then((response) => {
+                setFirstName(response.data.firstName);
+                setLastName(response.data.lastName);
+                setEmail(response.data.email);
+            }).catch(error => {
+                console.error(error);
+            })
+        }
 
-    createEmployee(employee).then((response) => {
-      console.log(response.data);
-      navigator("/employees");
-    });
-  } 
+    }, [id])
 
+    function saveOrUpdateEmployee(e){
+        e.preventDefault();
+
+        if(validateForm()){
+
+            const employee = {firstName, lastName, email}
+            console.log(employee)
+
+            if(id){
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees');
+                }).catch(error => {
+                    console.error(error);
+                })
+            } else {
+                createEmployee(employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            }
+        }
+    }
+
+    function validateForm(){
+        let valid = true;
+
+        const errorsCopy = {... errors}
+
+        if(firstName.trim()){
+            errorsCopy.firstName = '';
+        } else {
+            errorsCopy.firstName = 'First name is required';
+            valid = false;
+        }
+
+        if(lastName.trim()){
+            errorsCopy.lastName = '';
+        } else {
+            errorsCopy.lastName = 'Last name is required';
+            valid = false;
+        }
+
+        if(email.trim()){
+            errorsCopy.email = '';
+        } else {
+            errorsCopy.email = 'Email is required';
+            valid = false;
+        }
+
+        setErrors(errorsCopy);
+        
+        return valid;
 
     }
 
-    
-
-  function validateForm() {
-    let valid = true;
-
-    const errorsCopy = {... errors} 
-
-    if(firstName.trim()) {
-        errorsCopy.firstName = '';
-    } else {
-        errorsCopy.firstName = 'First name is required';
-        valid = false;
+    function pageTitle(){
+        if(id){
+            return <h2 className='text-center'>Update Employee</h2>
+        }else{
+            return <h2 className='text-center'>Add Employee</h2>
+        }
     }
-
-    if(lastName.trim()) {
-        errorsCopy.lastName = '';
-    } else {
-        errorsCopy.lastName = 'Last name is required';
-        valid = false;
-    }
-
-    if(email.trim()) {
-        errorsCopy.email = '';
-    } else {
-        errorsCopy.email = 'The email is required';
-        valid = false;
-    }
-
-    setErrors(errorsCopy);
-
-    return valid;
-  }
-
-  
-
   return (
-    <div className="container">
-      <br />
-      <br />
-      <div className="row">
-        <div className="card col-md-6 offset-md-3 offset-md-3">
-          <h2 className="text-center">Add Employee</h2>
-          <div className="card-body">
-            <form>
-              <div className="form-group mb-2">
-                <label className="form-label">First name: </label>
-                <input
-                  type="text"
-                  placeholder="Enter employee's first name"
-                  name="firstName"
-                  value={firstName}
-                  className={`form-control ${ errors.firstName ? 'is-invalid': '' }`}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-                {errors.firstName && <div className="invalid-feedback">{errors.firstName}</ div> }
-              </div>
-              <div className="form-group mb-2">
-                <label className="form-label">Last name: </label>
-                <input
-                  type="text"
-                  placeholder="Enter employee's last name"
-                  name="lastName"
-                  value={lastName}
-                  className={`form-control ${ errors.lastName ? 'is-invalid': '' }`}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-                {errors.lastName && <div className="invalid-feedback">{errors.lastName}</ div> }
-              </div>
-              <div className="form-group mb-2">
-                <label className="form-label">Email address: </label>
-                <input
-                  type="email"
-                  placeholder="Enter employee's email address"
-                  name="email"
-                  value={email}
-                  className={`form-control ${ errors.email ? 'is-invalid': '' }`}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                {errors.email && <div className="invalid-feedback">{errors.email}</ div> }
-              </div>
-              <button className="btn btn-success" onClick={saveEmployee}>
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <div className='container'>
+        <br /> <br />
+        <div className='row'>
+            <div className='card col-md-6 offset-md-3 offset-md-3'>
+               {
+                    pageTitle()
+               }
+                <div className='card-body'>
+                    <form>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>First Name:</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee First Name'
+                                name='firstName'
+                                value={firstName}
+                                className={`form-control ${ errors.firstName ? 'is-invalid': '' }`}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            >
+                            </input>
+                            { errors.firstName && <div className='invalid-feedback'> { errors.firstName} </div> }
+                        </div>
 
-export default EmployeeComponent;
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Last Name:</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee Last Name'
+                                name='lastName'
+                                value={lastName}
+                                className={`form-control ${ errors.lastName ? 'is-invalid': '' }`}
+                                onChange={(e) => setLastName(e.target.value)}
+                            >
+                            </input>
+                            { errors.lastName && <div className='invalid-feedback'> { errors.lastName} </div> }
+                        </div>
+
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Email:</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee Email'
+                                name='email'
+                                value={email}
+                                className={`form-control ${ errors.email ? 'is-invalid': '' }`}
+                                onChange={(e) => setEmail(e.target.value)}
+                            >
+                            </input>
+                            { errors.email && <div className='invalid-feedback'> { errors.email} </div> }
+                        </div>
+
+                        <button className='btn btn-success' onClick={saveOrUpdateEmployee} >Submit</button>
+                    </form>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default EmployeeComponent
--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,51 +1,77 @@
-import React, {useEffect, useState} from 'react'
-import { listEmployees } from '../services/EmployeeService'
+import React, { useEffect, useState } from 'react'
+import { listEmployees, deleteEmployee } from '../services/EmployeeService'
 import { useNavigate } from 'react-router-dom'
 
 const ListEmployeeComponent = () => {
 
-   const [employees, setEmployees] = useState([]);
-   const navigator = useNavigate();
+    const [employees, setEmployees] = useState([])
 
-   useEffect(() => {
-    listEmployees().then((response) => {
-        setEmployees(response.data)
-    }).catch(error => {
-        console.error(error);
-    })
-   }, [])
+    const navigator = useNavigate();
 
-   function addNewEmployee() {
+    useEffect(() => {
+        getAllEmployees();
+    }, [])
+
+    function getAllEmployees() {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+    function addNewEmployee(){
         navigator('/add-employee')
-   }
+    }
 
-    return (
-        <div className='container'>
-            <h2 className='text-center'>List of Employees</h2>
-            <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
-            <table className='table table-striped table-bordered'>
-                <thead>
-                    <tr>
-                        <th>Employee Id</th>
-                        <th>Employee First Name</th>
-                        <th>Employee Last Name</th>
-                        <th>Employee Email Id</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map(employee => 
+    function updateEmployee(id) {
+        navigator(`/edit-employee/${id}`)
+    }
+
+    function removeEmployee(id){
+        console.log(id);
+
+        deleteEmployee(id).then((response) =>{
+            getAllEmployees();
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+  return (
+    <div className='container'>
+
+        <h2 className='text-center'>List of Employees</h2>
+        <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
+        <table className='table table-striped table-bordered'>
+            <thead>
+                <tr>
+                    <th>Employee Id</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email Id</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    employees.map(employee => 
                         <tr key={employee.id}>
                             <td>{employee.id}</td>
                             <td>{employee.firstName}</td>
                             <td>{employee.lastName}</td>
                             <td>{employee.email}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-
-        </div>
-    )
+                            <td>
+                                <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
+                                <button className='btn btn-danger' onClick={() => removeEmployee(employee.id)}
+                                    style={{marginLeft: '10px'}}
+                                >Delete</button>
+                            </td>
+                        </tr>)
+                }
+            </tbody>
+        </table>
+    </div>
+  )
 }
 
 export default ListEmployeeComponent
